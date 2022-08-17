dc462d3aec1e588295f7e03b7d20b568d9deec49
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-17 17:02:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const div = document.getElementById("main");
+div.remove();
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+const yourName = "Nicholas njeru";
+newHeader.textContent = "YOUR-NAME is the champion";
